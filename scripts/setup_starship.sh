d11e80220567c652d947f22b5b988bbf32fa7a2c
Kevin Ulliac <55131275+kevanoullio@users.noreply.github.com>
2025-02-18 00:55:40 -0700

    Update setup_starship.sh

--- scripts/setup_starship.sh
+++ scripts/setup_starship.sh
@@ -7,9 +7,6 @@ sudo apt install -y curl
 # Configure and install starship prompt
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-# Clone my dotfiles repository
-git clone https://github.com/kevanoullio/dotfiles-linux.git ~/dotfiles
-
 # Copy starship.toml configuration file
 mkdir -p ~/.config
 cp ~/dotfiles-linux/.config/starship.toml ~/.config/starship.toml
