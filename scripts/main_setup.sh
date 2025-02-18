e535fe640ca11ed716b3631a6f36fd9c1940e97d
Kevin Ulliac <55131275+kevanoullio@users.noreply.github.com>
2025-02-18 00:45:57 -0700

    Update main_setup.sh

--- scripts/main_setup.sh
+++ scripts/main_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Run individual setup scripts
-bash ~/dotfiles/scripts/setup_python.sh
-bash ~/dotfiles/scripts/setup_c.sh
+bash ~/dotfiles/scripts/setup_common.sh
 bash ~/dotfiles/scripts/setup_nvim.sh
 bash ~/dotfiles/scripts/setup_starship.sh
+bash ~/dotfiles/scripts/setup_c.sh
+bash ~/dotfiles/scripts/setup_python.sh
 
 echo "All setups complete!"
