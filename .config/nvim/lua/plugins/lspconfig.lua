f5486b5bb45bad10a3db6f9248db6cd2cd990bb4
Kevin Ulliac <kevanoullio@outlook.com>
2025-02-19 22:17:38 -0700

    Added lspconfig and completions plugins.

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -5,24 +5,39 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
-            })
-        end
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+--         config = function()
+--             require("mason-lspconfig").setup({
+--                 ensure_installed = { "lua_ls" }
+--             })
+--         end,
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            lspconfig.tsserver.setup({
+                capabilities = capabilities
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
         end
     }
 }
