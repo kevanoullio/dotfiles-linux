096bf0a57a592da65decf50d0efb86b66118ce5c
Kevin Ulliac <kevanoullio@outlook.com>
2025-08-09 15:01:30 -0600

    Refactored c dev setup script using new utils.

--- scripts/setup_c.sh
+++ scripts/setup_c.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Update package list and install prerequisites
+# Import helper functions
+source "$(dirname "$0")/utils.sh"
+
+# Begin setup message
+env_title="C development"
+env_verb="setup"
+echo "Starting $env_title $env_verb..."
+
+# Update package list
 sudo apt update
+
+# Install C development tools
 sudo apt install -y build-essential gdb valgrind cmake
 
-echo "C development environment setup complete!"
-
+# Summary of versions
+print_env_setup_complete "$env_title" "$env_verb" \
+    "GCC" "gcc --version | head -n1" \
+    "G++" "g++ --version | head -n1" \
+    "Make" "make --version | head -n1" \
+    "GDB" "gdb --version | head -n1" \
+    "Valgrind" "valgrind --version" \
+    "CMake" "cmake --version | head -n1" \
