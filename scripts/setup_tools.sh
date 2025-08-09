db1e3185855ef78e8f46e4c8e3237dad29c18d46
Kevin Ulliac <kevanoullio@outlook.com>
2025-08-09 15:11:27 -0600

    Refactored tools script using new utils.

--- scripts/setup_tools.sh
+++ scripts/setup_tools.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Import helper functions
+source "$(dirname "$0")/utils.sh"
+
+# Begin setup message
+env_title="Terminal Tools"
+env_verb="setup"
+echo "Starting $env_title $env_verb..."
+
 # Update package list
 sudo apt update
 
-# Install the various tools
-sudo apt install -y neofetch
+# Install necessary tools (extend as needed)
+sudo apt install -y curl git wget jq tree htop fzf ripgrep
 
-echo "Neofetch is now installed!"
-
+# Print completion message
+print_env_setup_complete "Tools" \
+    "Curl" "curl --version | head -n1" \
+    "Git" "git --version" \
+    "jq" "jq --version" \
+    "tree" "tree --version" \
+    "htop" "htop --version" \
+    "fzf" "fzf --version" \
+    "ripgrep" "rg --version | head -n1"
