64a5a9134b3ffe7682b74ed1490ce612f4a0f83f
Kevin Ulliac <kevanoullio@outlook.com>
2026-01-09 12:12:20 -0700

    Updated nerd font installation, added dedicated script, updated pop_os script.

--- scripts/linux/pop_os.sh
+++ scripts/linux/pop_os.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Import helper functions
-source "$(dirname "$0")/../utils.sh"
+# Import helper functions if needed
+if [ -f "$(dirname "$0")/../utils.sh" ]; then
+    source "$(dirname "$0")/../utils.sh"
+fi
 
 # Begin setup message
 env_title="Font"
 env_verb="install"
 echo "Starting $env_title $env_verb..."
 
-# Install CaskaydiaMono Nerd Font to get Font Awesome, Material Design, Devicons, etc
-FONT_FOLDER="CascadiaMono"
-FONT_DIR="$HOME/.local/share/fonts"
-FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CascadiaMono.zip"
-
-install_from_zip "$FONT_URL" "$FONT_DIR" "$FONT_FOLDER"
-
-# Print completion message
-print_completion_message "$env_title" "$env_verb" \
-    "Font" "fc-list | grep -i 'CaskaydiaMono' | head -n1"
-
+# Install CaskaydiaMono Nerd Font using dedicated script
+"$(dirname "$0")/../fonts/install_nerd_font.sh"
 
 # Configure Pop_OS! settings
 echo "Configuring Pop_OS! settings..."
