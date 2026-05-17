69f0d352729e999910ca3c7d95214d087ee25b50
Kevin Ulliac <kevanoullio@outlook.com>
2026-05-16 21:32:31 -0600

    Moved and improved copy_file() and copy_directory() functions into utils.sh file, created create_directory() function, updated ~ to $HOME.

--- scripts/cli_tools/setup_nvim.sh
+++ scripts/cli_tools/setup_nvim.sh
@@ -23,57 +23,22 @@ pm_install neovim ripgrep unzip npm || { echo "Failed to install Neovim dependen
 # npm is a dependency for installing prettier via :Mason (auto formatter)
 
 # Create necessary directories for Neovim
-mkdir -p ~/.config/nvim/lua/config ~/.config/nvim/lua/plugins
+mkdir -p $HOME/.config/nvim/lua/config $HOME/.config/nvim/lua/plugins
 
 # Get the directory of the script
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-# Function to copy files with prompt
-copy_file() {
-    local src=$1
-    local dest=$2
-
-    if [ -d "$src" ]; then
-        cp -r "$src" "$dest" && echo "Copied directory $src to $dest" || echo "Error copying directory $src to $dest"
-    elif [ -f "$dest" ]; then
-        read -p "$dest already exists. Do you want to overwrite it? (y/n): " choice
-        case "$choice" in
-            y|Y ) cp -u "$src" "$dest" && echo "Copied $src to $dest" || echo "Error copying $src to $dest";;
-            n|N ) echo "Skipped $src";;
-            * ) echo "Invalid choice. Skipped $src";;
-        esac
-    else
-        cp -u "$src" "$dest" && echo "Copied $src to $dest" || echo "Error copying $src to $dest"
-    fi
-}
-
-# Function to copy all files in a directory recursively
-copy_directory() {
-    local src_dir=$1
-    local dest_dir=$2
-
-    mkdir -p "$dest_dir"
-    for src_file in "$src_dir"/*; do
-        local dest_file="$dest_dir/$(basename "$src_file")"
-        if [ -d "$src_file" ]; then
-            copy_directory "$src_file" "$dest_file"
-        else
-            copy_file "$src_file" "$dest_file"
-        fi
-    done
-}
-
 # Copy my Neovim configuration files
-copy_file "$SCRIPT_DIR/../../home.config/nvim/init.lua" ~/.config/nvim/init.lua
-copy_directory "$SCRIPT_DIR/../../home.config/nvim/lua/config" ~/.config/nvim/lua/config
-copy_directory "$SCRIPT_DIR/../../home.config/nvim/lua/plugins" ~/.config/nvim/lua/plugins
+copy_file "$SCRIPT_DIR/../../home.config/nvim/init.lua" $HOME/.config/nvim/init.lua
+copy_directory "$SCRIPT_DIR/../../home.config/nvim/lua/config" $HOME/.config/nvim/lua/config
+copy_directory "$SCRIPT_DIR/../../home.config/nvim/lua/plugins" $HOME/.config/nvim/lua/plugins
 
 # lazy.nvim package manager: https://github.com/folke/lazy.nvim
 # Install lazy.nvim package manager
-if [ -d ~/.local/share/nvim/site/pack/packer/start/lazy.nvim ]; then
+if [ -d $HOME/.local/share/nvim/site/pack/packer/start/lazy.nvim ]; then
     echo "lazy.nvim already installed, skipping clone."
 else
-    git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/site/pack/packer/start/lazy.nvim && echo "Cloned lazy.nvim" || echo "Error cloning lazy.nvim"
+    git clone https://github.com/folke/lazy.nvim.git $HOME/.local/share/nvim/site/pack/packer/start/lazy.nvim && echo "Cloned lazy.nvim" || echo "Error cloning lazy.nvim"
 fi
 
 echo "Neovim setup complete!"
--- scripts/terminals/setup_tmux.sh
+++ scripts/terminals/setup_tmux.sh
@@ -14,7 +14,8 @@ echo "Using package manager: $PKG_MANAGER"
 
 # Update package list and install tmux and session management plugins
 pm_update || { echo "Package list update failed" >&2; exit 1; }
-pm_install tmux xclip || { echo "Failed to install tmux" >&2; exit 1; }
+pm_install tmux || { echo "Failed to install tmux" >&2; exit 1; }
+pm_install xclip || { echo "Failed to install xclip" >&2; exit 1; }
 
 # Install tmux plugin manager and session management plugins if not present
 TPM_DIR="$HOME/.tmux/plugins/tpm"
@@ -26,8 +27,11 @@ fi
 # git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
 # git clone https://github.com/omerxx/tmux-sessionx ~/.tmux/plugins/tmux-sessionx
 
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 # Copy my tmux configuration file
-copy_file "$SCRIPT_DIR/../../home/.tmux.conf" ~/.tmux.conf
+copy_file "$SCRIPT_DIR/../../home/.tmux.conf" "$HOME/.tmux.conf"
 
 # Print completion message
 print_completion_message "$env_title" "$env_verb" \
@@ -38,7 +42,8 @@ print_completion_message "$env_title" "$env_verb" \
 echo "xclip allows you to copy text from tmux to your system's clipboard.
 Enter copy mode: Ctrl + B followed by [, press y after selecting text to copy it directly to system clipboard."
 
-echo "\nTo enable automatic tmux session saving and restoring, the following plugins are used:"
+echo ""
+echo "To enable automatic tmux session saving and restoring, the following plugins are used:"
 echo "- tmux-resurrect: https://github.com/tmux-plugins/tmux-resurrect"
 echo "- tmux-continuum: https://github.com/tmux-plugins/tmux-continuum"
 echo "- tmux-sessionx: https://github.com/omerxx/tmux-sessionx (optional, for advanced session management)"
